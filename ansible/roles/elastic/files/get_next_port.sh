2d127ec5eccffd32dc4abf2f0d99865b6f36eb1b
Chaitanya Akkineni <cakkineni@gmail.com>
2016-12-03 15:50:11 -0600

    getting next port

--- ansible/roles/elastic/files/get_next_port.sh
+++ ansible/roles/elastic/files/get_next_port.sh
@@ -3,7 +3,7 @@ lower_port=$1
 upper_port=$2
 ports="$(netstat -anlp | grep 'LISTEN ')"
 for (( port = lower_port ; port <= upper_port ; port++ )); do
-    echo $ports | grep $port
+    echo $ports | grep $port > /dev/null 2>&1
     if [[ "$?" != 0 ]]; then
         echo $port
 	    break;
